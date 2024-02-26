83c2eb308e129c8a6472a8d096d3ed0ce65a8f77
Steven Goff <steveng9@gmail.com>
2024-02-25 21:56:17 -0600

    initial checkin

---
+++ lib/src/sample_feature/sample_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// A placeholder class that represents an entity or model.
+class SampleItem {
+  const SampleItem(this.id, this.name, this.icon);
+
+  final int id;
+  final String name;
+  final IconData icon;
+  // final Icon icon;
+}
